b8c668786a370f31470928eaf65ae0bbaeda1d07
Donsimeon3 <dominicsimeon6@gmail.com>
2023-09-11 13:38:57 -0700

    structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,18 +9,16 @@
  *
  * Description: Longer description
  */
-
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 
 /**
- * dog_t: typedef for struct dog
+ * dog_t - typedef for struct dog
  */
-
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
@@ -31,3 +29,4 @@ char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 
 #endif
+
